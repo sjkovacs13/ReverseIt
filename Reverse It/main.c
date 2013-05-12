f9404bbb7593494906536d55ed814698e6cbbe75
Sam Kovacs <sjkovacs13@pthsd.net>
2013-05-12 13:32:48 -0400

    Initial Commit

---
+++ Reverse It/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Reverse It
+//
+//  Created by Sam Kovacs on 5/12/13.
+//  Copyright (c) 2013 Sam Kovacs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
